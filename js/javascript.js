2f09f10f695a5f94679ead368160987fc2a5570c
jcarlos9h <al9omeda@gmail.com>
2022-05-28 23:42:43 -0300

    modifique el index html con js todo otra vez

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,10 @@
 // cree el div contenedor del nombre
+let titulo=document.querySelector(".titulo");
 
 let contenedorTitulo=document.createElement("div");
 contenedorTitulo.className="header";
 
-document.body.appendChild(contenedorTitulo)
+titulo.appendChild(contenedorTitulo)
 
 let h1= document.createElement("h1");
     h1.id="titulo";
@@ -12,35 +13,13 @@ let h1= document.createElement("h1");
 let contenidoTitulo=document.createTextNode("Juan Carlos Algomeda");
     h1.appendChild(contenidoTitulo);
 
-// cree el div contenedor de las dos columnas
+// crear contenido de datos personales   
+document.querySelector(".dp").innerHTML="DATOS PERSONALES"; 
+document.querySelector(".nombre").innerHTML="Juan Carlos Algomeda";
+document.querySelector(".direccion").innerHTML="Ciudad Autonoma de Buenos Aires";
+document.querySelector(".phone").innerHTML="1163090865";
+document.querySelector(".mail").innerHTML="algomeda@gmail.com";
 
-let  contenedor=  document.createElement("div");
-    contenedor.className="contenedor";
-
-    document.body.appendChild(contenedor);
-
-//haciendo los dos div principales de la col1 
-let columna1=document.querySelector(".contenedor");
-
-let contenedorUno=document.createElement("div");
-    contenedorUno.className="cont-1";
-    
-    columna1.appendChild(contenedorUno);
-
-    //contenido de la columna 1
-     let imagen=document.querySelector(".cont-1");
-
-      let  imgPerfil=document.createElement("img");
-           imgPerfil.className="img";
-           imgPerfil.id="imagen";
-           imgPerfil.src='/img/IMG_20210626_002243_100.jpg';
-           imagen.appendChild(imgPerfil)
-    
-
-    // creando div principal de la col2
-    let columna2=document.querySelector(".contenedor");
-
-    let contenedorDos=document.createElement("div");
-        contenedorDos.className="cont-2";
-
-        columna2.appendChild(contenedorDos);
+//crear contenido de las otras dos secciones
+document.querySelector(".intereses").innerHTML="Intereses";
+document.querySelector(".idiomas").innerHTML="Idiomas"
